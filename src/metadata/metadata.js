1aefa6f046967fc77a03c72ad8627929c5b3cc80
IanD1013 <ian.tung1013@gmail.com>
2025-04-02 22:47:51 +1300

    Add some experiences, format education section

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'Full Stack Junior Developer',
   facts: {
     'Residence': `<a href="https://goo.gl/maps/kToTNMXQgnmXDa5e8">
-                  <i class="fa fa-home"></i> Auckland | open to relocate
+                  <i class="fa fa-home"></i>Auckland | open to relocate
                 </a>`,
     'LinkedIn': `<a href="https://www.linkedin.com/in/iandong1013/">
                   <i class="fa fa-linkedin"></i> iandong1013
@@ -22,33 +22,33 @@ module.exports = {
     {
       name: 'Technical Skills',
       examples: [
-        'Microsoft stack: C#, ASP.NET Core, Web API, MVC, EntityFramework, .NET MAUI',
-        'Modern web stack: JavaScript, TypeScript, NodeJS, React, Redux, NextJS, HTML5, CSS3, MongoDB, Vitest',
-        'UI/UX: Figma, Shadcn UI, Tailwind CSS, Material UI, Chakra UI, Bootstrap',
-        'Infrastructure: Azure, AWS, SQL Server, Docker, Kubernetes',
-        'Other: Java, Spring Boot, Python, Django, C, Matlab, ArcGIS, Blender',
-        'Experienced in web development & integration using industry standard technologies and frameworks',
-        'Experienced with all stages of the Software Development Life cycle and agile methodologies',
-        'Experienced and knowledge of best practices for software design and architecture'
+        'Microsoft stack: C#, ASP.NET Core, Web API, MVC, EntityFramework, .NET MAUI.',
+        'Modern web stack: JavaScript, TypeScript, NodeJS, React, Redux, NextJS, HTML5, CSS3, MongoDB, Vitest.',
+        'UI/UX: Figma, Shadcn UI, Tailwind CSS, Material UI, Chakra UI, Bootstrap.',
+        'Infrastructure: Azure, AWS, SQL Server, Docker, Kubernetes.',
+        'Other: Java, Spring Boot, Python, Django, C, Matlab, ArcGIS, Blender.',
+        'Experienced in web development & integration using industry standard technologies and frameworks.',
+        'Experienced with all stages of the Software Development Life cycle and agile methodologies.',
+        'Experienced and knowledge of best practices for software design and architecture.'
       ]
     },
     {
       name: 'Interpersonal and Communication Skills',
       examples: [
-        'Client facing skills gained as consultant and IT support',
-        'Excellent verbal and written communication skills',
-        'Confident in delivering business or team presentations',
-        'Experienced in writing detailed specifications and various forms of documentation',
-        'Fluent in English and Mandarin'
+        'Client facing skills gained as consultant and IT support.',
+        'Excellent verbal and written communication skills.',
+        'Confident in delivering business or team presentations.',
+        'Experienced in writing detailed specifications and various forms of documentation.',
+        'Fluent in English and Mandarin.'
       ]
     },
     {
       name: 'Learning Skills',
       examples: [
-        'Adapting very quickly to various working environments',
-        'Build up relevant domain knowledge in a short time frame',
-        'Confident in learning and picking up new technology stacks and frameworks',
-        'Work on side projects in free time'
+        'Adapting very quickly to various working environments.',
+        'Build up relevant domain knowledge in a short time frame.',
+        'Confident in learning and picking up new technology stacks and frameworks.',
+        'Work on side projects in free time.'
       ]
     }
   ],
@@ -79,17 +79,44 @@ module.exports = {
           title: 'IT Support Technician (Part-time)',
           start: 'Mar 2024',
           end: 'Current',
-          blurb: 'Member of a high performing team that delivers technical support to Civix engineers, surveyors, planners, and administrative staff, resolving hardware and software issues',
+          blurb: 'Member of a high performing team that delivers technical support to Civix engineers, surveyors, planners, and administrative staff, resolving hardware and software issues.',
           items: [
-            'Test1',
-            'Test2',
-            'Test3'
+            'Automated routine IT tasks using Pulseway and custom scripts (PowerShell, batch) to improve efficiency and reduce manual intervention.',
+            'Promoted the use of an online issue tracker and maintained a comprehensive IT knowledge base by creating and updating documentation, streamlining support and improving response times.',
+            'Facilitated staff onboarding and offboarding, including laptop/workstation setup and access provisioning.',
+            'Installed, configured, and maintained computer hardware, software, networks, servers, and peripherals.',
+            'Resolved hardware, software, and network connectivity issues via phone, video call, ticketing system, and in-person support.',
+            'Collaborated with cross-functional teams to ensure smooth IT operations across departments.',
+            'Demonstrated strong multitasking and communication skills while managing concurrent support requests.'           
           ]
         }
       ],
       badges: ['Powershell', 'Bash', 'Pulseway', 'Microsoft 365 System Administrator', 'Entra ID', 'Windows', 'Linux', 'Microsoft Teams'], 
     },
-
+    {
+      company: 'Allsite.ai',
+      location: 'Auckland',
+      roles: [
+        {
+          title: 'Software Engineering Intern (Full-time)',
+          start: 'Nov 2023',
+          end: 'Feb 2024',
+          blurb: 'Developed and implemented advanced geospatial analysis solutions using machine learning, computer vision, and GIS technologies to optimize urban planning and infrastructure design processes.',
+          items: [
+            'Developed a Python script using ArcPy and R-trees to reduce false positives in identifying driveways.',
+            'Built a data pipeline to process 400GB of raw parcel data, integrating road information and supporting multiple file formats including zip and gdb.',
+            'Analyzed housing parcel connectivity with Graph Neural Networks to support layout design and accurately determine centroid locations.',
+            'Modified a UNet++ model to detect impervious surfaces in New Zealand with 95% accuracy.',
+            'Created a predictive function to interpolate and forecast rainfall data across multiple years.',
+            'Implemented a kinematic model for vehicle path tracking to optimize road width and enable dynamic control.',
+            'Engineered a Python script utilizing Unity, Blender, and Esri SDKs to automate the creation of dynamic promotional videos showcasing ArcGIS 3D scenes.',
+            'Contributed to internal knowledge sharing by presenting domain concepts and system architecture in a team-wide learning session, promoting cross-team understanding and collaboration.',
+            'Delivered a final internship presentation to the Allsite.ai team and Civix engineers, showcasing project outcomes and impact; recognized by company leadership in a public post celebrating intern contributions.'
+          ]
+        }
+      ],
+      badges: ['Python', 'ArcPy', 'Machine Learning', 'CNN', 'GNN', 'PyTorch', 'Unity', 'Blender', 'ArcGIS', 'ArcGIS Maps SDK for Unity', 'Data Pipeline'], 
+    },
   ],
   interests: ['Technology', 'Sports & Fitness', 'History', 'Politics', 'Philosophy']
 };
